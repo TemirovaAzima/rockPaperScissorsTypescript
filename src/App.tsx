b70f3195717e68ececf680c05424e69bedce7a67
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-18 17:28:45 +0500

    localeStorage added

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
 import React from 'react'
-import {computerChoice} from './rockPaperScissors.ts'
+// import {computerChoice} from './rockPaperScissors.ts'
+import {playGame, score} from './rockPaperScissors.ts'
 
 const App: React.FC = () => {
-    const playGame = (userChoice: string): void => {
-        const computer = computerChoice();
-        let result: string = '';
-
-        if (userChoice === 'rock') {
-            if (computer === 'Rock') result = 'Tie'
-            else if (computer === 'Paper') result = 'U lose'
-            else result = 'U win'
-
-        } else if (userChoice === 'paper') {
-            if (computer === 'Rock') result = 'U win'
-            else if (computer === 'Paper') result = "Tie"
-            else result = "U lose"
-
-        } else if (userChoice === 'scissors') {
-            if (computer === 'Rock') result = 'U lose'
-            else if (computer === 'Paper') result = "U win"
-            else result = "Tie"
-        }
-        alert(
-            `U chose ${userChoice} . Computer chose ${computer}. Result is ${result}`
-        );
-    };
+    // const playGame = (userChoice: string): void => {
+    //     const computer = computerChoice();
+    //     let result: string = '';
+    //
+    //     if (userChoice === 'rock') {
+    //         if (computer === 'Rock') result = 'Tie'
+    //         else if (computer === 'Paper') result = 'U lose'
+    //         else result = 'U win'
+    //
+    //     } else if (userChoice === 'paper') {
+    //         if (computer === 'Rock') result = 'U win'
+    //         else if (computer === 'Paper') result = "Tie"
+    //         else result = "U lose"
+    //
+    //     } else if (userChoice === 'scissors') {
+    //         if (computer === 'Rock') result = 'U lose'
+    //         else if (computer === 'Paper') result = "U win"
+    //         else result = "Tie"
+    //     }
+    //     alert(
+    //         `U chose ${userChoice} . Computer chose ${computer}. Result is ${result}`
+    //     );
+    // };
 
     return (
         <div>
@@ -32,6 +33,14 @@ const App: React.FC = () => {
             <button onClick={() => playGame('rock')}>Rock</button>
             <button onClick={() => playGame('paper')}>Paper</button>
             <button onClick={() => playGame('scissors')}>Scissors</button>
+            <button onClick={() => {
+                score.wins = 0;
+                score.loses = 0;
+                score.tie = 0;
+                alert('Game is restarted')
+                localStorage.removeItem('score')
+            }}>Restart
+            </button>
         </div>
     );
 };
--- src/rockPaperScissors.ts
+++ src/rockPaperScissors.ts
@@ -1,3 +1,21 @@
+export const score = JSON.parse(localStorage.getItem('score')||'null') ||{
+    tie:0,
+    loses:0,
+    wins:0
+}
+// export let score = JSON.parse(localStorage.getItem('score'))
+// if(score===null) {
+//     score= {
+//         wins: 0,
+//         loses: 0,
+//         tie: 0
+//     }
+// }
+// export const score ={
+//     wins:0,
+//     loses:0,
+//     tie:0
+// }
 export function computerChoice(): string {
     const randomNumber: number= Math.random();
     let computer:string = ""
@@ -9,5 +27,37 @@ export function computerChoice(): string {
     } else {
         computer = "Scissors";
     }
-    return computer;
-}
+     return computer;
+ }
+
+export const playGame = (userChoice: string): void => {
+    const computer = computerChoice();
+    let result: string = '';
+    if (userChoice === 'rock') {
+        if (computer === 'Rock') result = 'Tie'
+        else if (computer === 'Paper') result = 'U lose'
+        else result = 'U win'
+
+    } else if (userChoice === 'paper') {
+        if (computer === 'Rock') result = 'U win'
+        else if (computer === 'Paper') result = "Tie"
+        else result = "U lose"
+
+    } else if (userChoice === 'scissors') {
+        if (computer === 'Rock') result = 'U lose'
+        else if (computer === 'Paper') result = "U win"
+        else result = "Tie"
+    }
+    // score update
+    if(result === 'Tie') score.tie++
+    else if (result === "U win") score.wins++
+    else if(result === "U lose") score.loses++
+    localStorage.setItem('score',JSON.stringify(score))
+    alert(
+        `U chose ${userChoice} . Computer chose ${computer}. Result is ${result}
+         Wins: ${score.wins}
+         Loses: ${score.loses}
+         Tie: ${score.tie}
+        `
+    );
+};
